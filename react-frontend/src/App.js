d02ff0e052f055189e0e34d4fbb5ce1578cc7cb0
Ahmad Othman <aothman156@gmail.com>
2022-07-04 02:56:27 +0300

    add:navbar

---
+++ react-frontend/src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import './App.css';
+import Main from './pages/main'
+import  Navbar from './components/Navbar'
+import  Filters from './components/Filters'
+import { IconName } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Filters />
+        <Routes>
+          <Route path='/main' element={Main}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/components/Navbar.js
@@ -0,0 +1,56 @@
+import './../style/navbar.css'
+import logo from './../assets/logo.jpg' 
+import word from './../assets/word.jpg' 
+import{FaSearch} from "react-icons/fa";
+import {FiGlobe} from "react-icons/fi"
+import {GiHamburgerMenu} from "react-icons/gi"
+import {IoMdPerson} from "react-icons/io"
+const Navbar = () => {
+
+    return(
+        
+        <nav className='nav'>
+            <div className='left'>
+                <div className='full-logo'>
+                    <a href='#'><imag src={logo} className="logo-imag" /></a>
+                    <a href='#'><imag src={word} className="logo-word" /></a>
+                </div>
+            </div>
+            <div className='middle'>
+                <div className='nav-menu'>
+                    <div className='texts'>
+                        <span>Anywhere</span>
+                        <span>Any week</span>
+                        <span>Add guests</span>
+                    </div>
+                    <div className='search-div'>
+                        <FaSearch className='search-btn' />
+                    </div>
+                </div>
+            </div>
+            <div className='right'>
+                <div className='rightside-elements'>
+                    <div className='host-btn'>
+                        <span>Become a Host</span>
+                    </div>
+                    <div className='globe'>
+                        <FiGlobe />
+                    </div>
+                    <div className='login-menu'>
+                        <div className='burger'>
+                            <GiHamburgerMenu />
+                        </div>
+                        <div className='profile-imag'>
+                            <IoMdPerson />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+
+       </nav>
+    )
+}
+
+export default Navbar;
